0bc0fcada820dab141843f973e6ac0eed291511b
Constantin Dumitru <dumitrubutco11@gmail.com>
2025-02-16 14:11:49 +0200

    navbar

--- src/pages/club/components/NavBar.tsx
+++ src/pages/club/components/NavBar.tsx
@@ -51,12 +51,12 @@ const NavBar: React.FC = () => {
   ])
 
   const menuItemVariants = {
-    hidden: { opacity: 0, y: 20 },
+    hidden: { opacity: 0, x: 20 },
     visible: (i: number) => ({
       opacity: 1,
-      y: 0,
+      x: 0,
       transition: {
-        delay: i * 0.1,
+        delay: i * 0.11,
         duration: 0.5,
         ease: "easeOut",
       },

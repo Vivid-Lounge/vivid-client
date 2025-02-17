f090c22f727fccdb0647330f829737c837096106
MarcuCtin <constantin-marcu.ciobanu@student.tuiasi.ro>
2025-02-17 18:22:50 +0200

    restaurant linking

--- src/pages/club/pages/MainPage/sections/HeroSection.tsx
+++ src/pages/club/pages/MainPage/sections/HeroSection.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Grid2, Typography, Box } from '@mui/material'
+import { Grid2, Typography, Box, Link } from '@mui/material'
 import { VividLogoIcon } from '../../../../../shared/icons'
 import Button from '../../../components/Button'
 import videohero from '../../../../../shared/videohero.mp4'
+import { Navigate, useNavigate } from 'react-router-dom'
 const HeroSection: React.FC = () => {
+	const navigate = useNavigate()
 	return (
 		<Grid2
 			container
@@ -83,9 +85,18 @@ const HeroSection: React.FC = () => {
 						fill: 'white',
 					}}
 				/>
-				<Button>
-					<Typography>restaurant</Typography>
-				</Button>
+				<Link
+					sx={{
+						textDecoration: 'none',
+						color: 'white',
+					}}
+					href='https://restaurant.lsaciasi.ro/'
+					target='_blank'
+				>
+					<Button>
+						<Typography>restaurant</Typography>
+					</Button>
+				</Link>
 			</Grid2>
 		</Grid2>
 	)

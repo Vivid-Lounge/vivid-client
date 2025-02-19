a1531b41369d75d86b67371d2303384e7954fa0c
chinezprost <playschan@gmail.com>
2025-02-19 21:28:00 +0200

    fixed service container

--- src/pages/club/pages/MainPage/components/ServiceContainer.tsx
+++ src/pages/club/pages/MainPage/components/ServiceContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import { Stack, Typography, Box } from '@mui/material'
 import { LoungeBarIcon } from '../../../../../shared/icons'
 
